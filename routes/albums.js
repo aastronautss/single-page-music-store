df9dae0a4500729f55e31e943b185b32ccef48ce
Tyler Guillen <tyguillen@gmail.com>
2016-06-08 20:15:41 -0400

    finish routes for albums/new and routes refactoring

--- routes/albums.js
+++ routes/albums.js
@@ -3,10 +3,28 @@ var path = require('path'),
     file_path = path.resolve(path.dirname(__dirname), 'data/albums.json');
 
 function getAlbums() {
-  return JSON.parse(fs.redFileSync(file_path, 'utf8'));
+  return JSON.parse(fs.readFileSync(file_path, 'utf8')).data;
+}
+
+function nextID() {
+  return JSON.parse(fs.readFileSync(file_path, 'utf8')).last_id + 1;
+}
+
+function writeAlbums(data) {
+  fs.writeFileSync(file_path, JSON.stringify(data), 'utf8');
 }
 
 module.exports = function(router) {
+  router.post('/albums', function(req, res) {
+    var album = req.body,
+        albums = getAlbums();
+
+    album.id = nextID();
+    albums.push(album);
+    writeAlbums({ last_id: album.id, data: albums });
+    res.json(album);
+  });
+
   router.get('/albums/new', function(req, res) {
     res.render('new');
   });
--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,13 @@ var fs = require('fs');
 var file_path = path.resolve(path.dirname(__dirname), 'data/albums.json');
 
 function getAlbums() {
-  return JSON.parse(fs.readFileSync(file_path, 'utf8'));
+  return JSON.parse(fs.readFileSync(file_path, 'utf8')).data;
 }
 
-
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Express',
-    albums: getAlbums()
+module.exports = function(router) {
+  router.get('/', function(req, res, next) {
+    res.render('index', {
+      albums: getAlbums()
+    });
   });
-});
-
-module.exports = router;
+}
