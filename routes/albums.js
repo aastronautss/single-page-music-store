70d390209d36225dddc195efa0e6cf5cbb3d6820
Tyler Guillen <tyguillen@gmail.com>
2016-06-13 23:21:01 -0400

    albums and new album views functional with backbone router

--- routes/albums.js
+++ routes/albums.js
@@ -1,31 +1,32 @@
-var path = require('path'),
-    fs = require('fs'),
-    file_path = path.resolve(path.dirname(__dirname), 'data/albums.json');
-
-function getAlbums() {
-  return JSON.parse(fs.readFileSync(file_path, 'utf8')).data;
-}
-
-function nextID() {
-  return JSON.parse(fs.readFileSync(file_path, 'utf8')).last_id + 1;
-}
-
-function writeAlbums(data) {
-  fs.writeFileSync(file_path, JSON.stringify(data), 'utf8');
-}
+var Albums = require('albums');
 
 module.exports = function(router) {
   router.post('/albums', function(req, res) {
     var album = req.body,
-        albums = getAlbums();
+        albums = Albums.get();
 
-    album.id = nextID();
+    album.id = Albums.nextID();
     albums.push(album);
-    writeAlbums({ last_id: album.id, data: albums });
+    Albums.set({ last_id: album.id, data: albums });
     res.json(album);
   });
 
+  router.put('/albums', function(req, res) {
+    var album = req.body;
+    album = Albums.put(album);
+
+    res.json(album);
+  });
+
+  router.delete('/albums', function(req, res) {
+    var album = req.body;
+    Albums.delete(album);
+    res.status(200).end();
+  });
+
   router.get('/albums/new', function(req, res) {
-    res.render('new');
+    res.render('new', {
+      albums: Albums.get()
+    });
   });
 }
